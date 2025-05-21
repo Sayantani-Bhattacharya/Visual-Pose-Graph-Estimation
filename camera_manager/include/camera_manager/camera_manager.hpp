6a331a431aa9d96aeecb8fde713025d46d77c170
sayantani <sayantanibhattacharya2025@u.northwestern.edu>
2025-05-21 17:02:09 -0500

    "Base Structure."

--- camera_manager/include/camera_manager/camera_manager.hpp
+++ camera_manager/include/camera_manager/camera_manager.hpp
@@ -1,12 +1,27 @@
 #ifndef CAMERA_MANAGER_HPP
 #define CAMERA_MANAGER_HPP
 #include <rclcpp/rclcpp.hpp>
-
+#include <sensor_msgs/msg/image.hpp>
+#include <opencv2/opencv.hpp>
 
 class CameraManager : public rclcpp::Node {
 public:
   CameraManager();
   ~CameraManager() = default;
-};
+  void FeatureExtractor();
+  void CameraPoseEstimation();
+  void GraphBuilder();
+  // void CameraPoseGraphOptimization();
+  // void LoopClosureDetector();
 
+
+private:
+  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr camera_sub;
+  
+  // cv::frame mFrame;
+
+
+  void image_callback(const sensor_msgs::msg::Image::SharedPtr msg);
+
+};
 #endif // !CAMERA_MANAGER_HPP
--- camera_manager/src/camera_manager.cpp
+++ camera_manager/src/camera_manager.cpp
@@ -1,9 +1,24 @@
 #include <rclcpp/rclcpp.hpp>
 #include "camera_manager.hpp"
 
+
+// Pub: Node, Edges
+// Sub: RS images.
+
 CameraManager::CameraManager() : Node("camera_manager") {
   RCLCPP_INFO(this->get_logger(), "Camera Manager Node Initialized");
+
+  camera_sub = this->create_subscription<sensor_msgs::msg::Image>("camera/image", 10, std::bind(&CameraManager::image_callback, this, std::placeholders::_1));
+
 }
+
+void image_callback(const sensor_msgs::msg::Image::SharedPtr msg)
+{
+  // RCLCPP_INFO(this->get_logger(), "Received image with width: %d, height: %d", msg->width, msg->height);
+  // /convert msg to cv frame.
+}
+
+
 
 int main(int argc, char* argv[]) {
   rclcpp::init(argc, argv);
