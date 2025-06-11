fe15c6b2c42340a1ee466b7b537628e10c06939b
Sharwin24 <sharwin24@gmail.com>
2025-06-10 20:06:29 -0500

    Created teleop to publish wheelcommands from keyboard

---
+++ camera_manager/src/teleop.py
@@ -0,0 +1,303 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2011, Willow Garage, Inc.
+# All rights reserved.
+#
+# Software License Agreement (BSD License 2.0)
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of {copyright_holder} nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# Author: Darby Lim
+
+import os
+import select
+import sys
+
+from nuturtlebot_msgs.msg import WheelCommands
+
+from geometry_msgs.msg import Twist
+from geometry_msgs.msg import TwistStamped
+import rclpy
+from rclpy.clock import Clock
+from rclpy.qos import QoSProfile
+
+if os.name == 'nt':
+    import msvcrt
+else:
+    import termios
+    import tty
+
+BURGER_MAX_LIN_VEL = 0.22
+BURGER_MAX_ANG_VEL = 2.84
+
+WAFFLE_MAX_LIN_VEL = 0.26
+WAFFLE_MAX_ANG_VEL = 1.82
+
+LIN_VEL_STEP_SIZE = 0.01
+ANG_VEL_STEP_SIZE = 0.1
+
+TURTLEBOT3_MODEL = os.environ['TURTLEBOT3_MODEL']
+
+msg = """
+Control Your TurtleBot3!
+---------------------------
+Moving around:
+        w
+   a    s    d
+        x
+
+w/x : increase/decrease linear velocity (Burger : ~ 0.22, Waffle and Waffle Pi : ~ 0.26)
+a/d : increase/decrease angular velocity (Burger : ~ 2.84, Waffle and Waffle Pi : ~ 1.82)
+
+space key, s : force stop
+
+CTRL-C to quit
+"""
+
+e = """
+Communications Failed
+"""
+
+# WheelCommands MSG
+# Left and right wheel velocity, in "motor command units" (mcu)
+# For the turtlebot, each motor can be command with an integer velocity of between
+# -265 mcu and 265 mcu, and 1 mcu = 0.024 rad/sec
+# int32 left_velocity
+# int32 right_velocity
+# The wheel_cmd messages are integer values between -265 and 265
+# and are proportional to the maximum rotational velocity of the motor(see Specifications and A.8).
+
+
+def Twist2WheelCommands(twist: Twist | TwistStamped) -> WheelCommands:
+    """
+    Convert a Twist or TwistStamped message to WheelCommands.
+    """
+    if isinstance(twist, TwistStamped):
+        twist = twist.twist
+    MCU = 0.024  # [rad/sec] per mcu
+    # Convert linear and angular velocities to wheel velocities
+    wheel_seperation = 160  # [mm] between wheels
+    left = twist.linear.x - (twist.angular.z * wheel_seperation / 2.0)
+    right = twist.linear.x + (twist.angular.z * wheel_seperation / 2.0)
+    # Convert to mcu
+    left_mcu = int(left / MCU)
+    right_mcu = int(right / MCU)
+    # Constrain to [-265, 265]
+    left_mcu = max(-265, min(265, left_mcu))
+    right_mcu = max(-265, min(265, right_mcu))
+    # Create WheelCommands message
+    wheel_commands = WheelCommands()
+    wheel_commands.left_velocity = left_mcu
+    wheel_commands.right_velocity = right_mcu
+    return wheel_commands
+
+
+def get_key(settings):
+    if os.name == 'nt':
+        return msvcrt.getch().decode('utf-8')
+    tty.setraw(sys.stdin.fileno())
+    rlist, _, _ = select.select([sys.stdin], [], [], 0.1)
+    if rlist:
+        key = sys.stdin.read(1)
+    else:
+        key = ''
+
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    return key
+
+
+def print_vels(target_linear_velocity, target_angular_velocity):
+    print('currently:\tlinear velocity {0}\t angular velocity {1} '.format(
+        target_linear_velocity,
+        target_angular_velocity))
+
+
+def make_simple_profile(output_vel, input_vel, slop):
+    if input_vel > output_vel:
+        output_vel = min(input_vel, output_vel + slop)
+    elif input_vel < output_vel:
+        output_vel = max(input_vel, output_vel - slop)
+    else:
+        output_vel = input_vel
+
+    return output_vel
+
+
+def constrain(input_vel, low_bound, high_bound):
+    if input_vel < low_bound:
+        input_vel = low_bound
+    elif input_vel > high_bound:
+        input_vel = high_bound
+    else:
+        input_vel = input_vel
+
+    return input_vel
+
+
+def check_linear_limit_velocity(velocity):
+    if TURTLEBOT3_MODEL == 'burger':
+        return constrain(velocity, -BURGER_MAX_LIN_VEL, BURGER_MAX_LIN_VEL)
+    else:
+        return constrain(velocity, -WAFFLE_MAX_LIN_VEL, WAFFLE_MAX_LIN_VEL)
+
+
+def check_angular_limit_velocity(velocity):
+    if TURTLEBOT3_MODEL == 'burger':
+        return constrain(velocity, -BURGER_MAX_ANG_VEL, BURGER_MAX_ANG_VEL)
+    else:
+        return constrain(velocity, -WAFFLE_MAX_ANG_VEL, WAFFLE_MAX_ANG_VEL)
+
+
+def main():
+    settings = None
+    if os.name != 'nt':
+        settings = termios.tcgetattr(sys.stdin)
+
+    rclpy.init()
+    ROS_DISTRO = os.environ.get('ROS_DISTRO')
+    qos = QoSProfile(depth=10)
+    node = rclpy.create_node('teleop_keyboard')
+    if ROS_DISTRO == 'humble':
+        pub = node.create_publisher(Twist, 'cmd_vel', qos)
+    else:
+        pub = node.create_publisher(TwistStamped, 'cmd_vel', qos)
+
+    status = 0
+    target_linear_velocity = 0.0
+    target_angular_velocity = 0.0
+    control_linear_velocity = 0.0
+    control_angular_velocity = 0.0
+
+    try:
+        print(msg)
+        while (1):
+            key = get_key(settings)
+            if key == 'w':
+                target_linear_velocity =\
+                    check_linear_limit_velocity(
+                        target_linear_velocity + LIN_VEL_STEP_SIZE)
+                status = status + 1
+                print_vels(target_linear_velocity, target_angular_velocity)
+            elif key == 'x':
+                target_linear_velocity =\
+                    check_linear_limit_velocity(
+                        target_linear_velocity - LIN_VEL_STEP_SIZE)
+                status = status + 1
+                print_vels(target_linear_velocity, target_angular_velocity)
+            elif key == 'a':
+                target_angular_velocity =\
+                    check_angular_limit_velocity(
+                        target_angular_velocity + ANG_VEL_STEP_SIZE)
+                status = status + 1
+                print_vels(target_linear_velocity, target_angular_velocity)
+            elif key == 'd':
+                target_angular_velocity =\
+                    check_angular_limit_velocity(
+                        target_angular_velocity - ANG_VEL_STEP_SIZE)
+                status = status + 1
+                print_vels(target_linear_velocity, target_angular_velocity)
+            elif key == ' ' or key == 's':
+                target_linear_velocity = 0.0
+                control_linear_velocity = 0.0
+                target_angular_velocity = 0.0
+                control_angular_velocity = 0.0
+                print_vels(target_linear_velocity, target_angular_velocity)
+            else:
+                if (key == '\x03'):
+                    break
+
+            if status == 20:
+                print(msg)
+                status = 0
+
+            control_linear_velocity = make_simple_profile(
+                control_linear_velocity,
+                target_linear_velocity,
+                (LIN_VEL_STEP_SIZE / 2.0))
+
+            control_angular_velocity = make_simple_profile(
+                control_angular_velocity,
+                target_angular_velocity,
+                (ANG_VEL_STEP_SIZE / 2.0))
+
+            if ROS_DISTRO == 'humble':
+                twist = Twist()
+                twist.linear.x = control_linear_velocity
+                twist.linear.y = 0.0
+                twist.linear.z = 0.0
+
+                twist.angular.x = 0.0
+                twist.angular.y = 0.0
+                twist.angular.z = control_angular_velocity
+
+                pub.publish(Twist2WheelCommands(twist))
+            else:
+                twist_stamped = TwistStamped()
+                twist_stamped.header.stamp = Clock().now().to_msg()
+                twist_stamped.header.frame_id = ''
+                twist_stamped.twist.linear.x = control_linear_velocity
+                twist_stamped.twist.linear.y = 0.0
+                twist_stamped.twist.linear.z = 0.0
+
+                twist_stamped.twist.angular.x = 0.0
+                twist_stamped.twist.angular.y = 0.0
+                twist_stamped.twist.angular.z = control_angular_velocity
+
+                pub.publish(Twist2WheelCommands(twist_stamped))
+
+    except Exception as e:
+        print(e)
+
+    finally:
+        if ROS_DISTRO == 'humble':
+            twist = Twist()
+            twist.linear.x = 0.0
+            twist.linear.y = 0.0
+            twist.linear.z = 0.0
+            twist.angular.x = 0.0
+            twist.angular.y = 0.0
+            twist.angular.z = 0.0
+            pub.publish(Twist2WheelCommands(twist))
+        else:
+            twist_stamped = TwistStamped()
+            twist_stamped.header.stamp = Clock().now().to_msg()
+            twist_stamped.header.frame_id = ''
+            twist_stamped.twist.linear.x = control_linear_velocity
+            twist_stamped.twist.linear.y = 0.0
+            twist_stamped.twist.linear.z = 0.0
+            twist_stamped.twist.angular.x = 0.0
+            twist_stamped.twist.angular.y = 0.0
+            twist_stamped.twist.angular.z = control_angular_velocity
+            pub.publish(Twist2WheelCommands(twist_stamped))
+
+        if os.name != 'nt':
+            termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+
+
+if __name__ == '__main__':
+    main()
